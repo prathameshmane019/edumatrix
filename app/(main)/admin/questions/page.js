23f49c02d93a7f25cfb070b69142f533f5d81310
12DeshmukhAish <113546938+12DeshmukhAish@users.noreply.github.com>
2025-01-12 11:44:04 +0530

    updated feedback modal

---
+++ app/(main)/admin/questions/page.js
@@ -0,0 +1,14 @@
+
+import QuestionForm from '@/app/components/feedback/question'
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className=''>
+    <QuestionForm/>
+    </div>
+  )
+}
+
+export default page
